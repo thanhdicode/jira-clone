a6acfc27eb52dc7ca4143f34e290267260d364ab
thanhdicode <thanhhdse184021@fpt.edu.vn>
2024-10-15 22:08:39 +0700

    change button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
+
 import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
-  <div>
-
+  <div className="flex flex-col items-center justify-center h-screen">
+<Button variant={"primary"} size={"lg"}  >
+  Primary
+</Button>
+    <Button variant={"destructive"}>
+      Primary
+    </Button>
+      <Button variant={"teritary"}>
+          Primary
+      </Button>
   </div>
   );
 }
